6f2e2051327ada6331a90bcd6072ec09791cf411
FeilongWu <50563812+FeilongWu@users.noreply.github.com>
2025-02-18 15:34:46 +0800

    Update main.py

--- E2LC/VCNet/main.py
+++ E2LC/VCNet/main.py
@@ -7,39 +7,34 @@ import json
 from torch.utils.data import DataLoader
 
 from models.dynamic_net import Vcnet, Drnet, TR
-from data.data import get_iter
 from utils.eval import *
+import pickle
 
 import argparse
 
 
 
-def load_data(dataset):
-    file = open('../data/' + dataset + '_metadata.json')
-    meta = json.load(file)
-    file.close()
-    v1 = meta['v1']
-    v2 = meta['v2']
-    v3 = meta['v3']
-
+def load_data(dataset):    
     x = []
     d = []
     y = []
-    file = '../data/' + dataset + '_simulate.csv'
+    ids = []
+    file = '../data/' + dataset + '.csv'
     with open(file) as file1:
         reader = csv.reader(file1, delimiter=',')
         for row in reader:
             #t.append(int(row[0]))
             d.append(float(row[1]))
             y.append(float(row[0]))
+            ids.append(float(row[-1]))
             temp = []
-            for entry in row[2:]:
+            for entry in row[2:-1]:
                 temp.append(float(entry))
             x.append(temp)
     x = np.array(x)
     d = np.array(d)
     y = np.array(y)
-    return x, d, y, [v1,v2,v3]
+    return x, d, y, ids
 
 
 
@@ -66,6 +61,8 @@ def save_checkpoint(state, checkpoint_dir='.'):
 # criterion
 def criterion(out, y, alpha=0.5, epsilon=1e-6):
     return ((out[1].squeeze() - y.squeeze())**2).mean() - alpha * torch.log(out[0] + epsilon).mean()
+#def criterion(out, y, alpha=0.5, epsilon=1e-6):
+#    return ((out[1].squeeze() - y.squeeze())**2).mean()
 
 def criterion_TR(out, trg, y, beta=1., epsilon=1e-6):
     # out[1] is Q
@@ -117,7 +114,7 @@ if __name__ == "__main__":
     # targeted regularization optimizer
     tr_wd = 5e-3
 
-    num_epoch = 500
+    num_epoch = 50
 
     # check val loss
     verbose = args.verbose
@@ -140,36 +137,42 @@ if __name__ == "__main__":
     grid = []
     MSE = []
 
-    dataset = 'eicu'
+    dataset = 'mimiciv_coag'
+    with open('../data/' + dataset + '_response_curve_calibrate.pickle', 'rb') as file:
+        response_data = pickle.load(file)
     out_path = './VCNet_' + dataset + '.txt'
     file = open(out_path, 'w')
     file.write('')
     file.close()
     test_ratio = 0.2
     batch_size = 150
-    hyperparameters = {'mimic':{'num_units':[60], 'lrs':[ 0.001],\
+    hyperparameters = {'mimiciii_mv':{'num_units':[44], 'lrs':[0.00005],\
                                 'alphas':[0.0], \
-                           'num_grids':[9]},\
-                       'eicu':{'num_units':[40], 'lrs':[0.001],\
-                                'alphas':[0.3,0.5,0.7], \
-                           'num_grids':[9,10,11]},\
-                       'ihdp_m50':{'num_units':[35,45,55], 'lrs':[0.001, 0.0001],\
-                                'alphas':[0.3,0.5,0.7], \
-                           'num_grids':[9,10,11]},\
-                       'synthetic_unbiased':{'num_units':[50], 'lrs':[0.001],\
-                                'alphas':[0], \
-                           'num_grids':[10]}}[dataset]
+                           'num_grids':[11]},\
+                       'mimiciv_mv':{'num_units':[34], 'lrs':[0.00005],\
+                                'alphas':[0.3], \
+                           'num_grids':[11]},\
+                       'mimiciii_seda':{'num_units':[46], 'lrs':[0.0003],\
+                                'alphas':[0.0], 'num_grids':[9]},\
+                       'mimiciv_seda':{'num_units':[32], 'lrs':[0.0003],\
+                                'alphas':[0.7], 'num_grids':[11]},\
+                       'mimiciv_coag':{'num_units':[38], 'lrs':[0.0003],\
+                                'alphas':[0.0], 'num_grids':[10]}}[dataset]
     replications = 5
 
 
     
     # choose from {'Tarnet', 'Tarnet_tr', 'Drnet', 'Drnet_tr', 'Vcnet', 'Vcnet_tr'}
-    method_list = [ 'Vcnet']
+    method_list = [ 'Vcnet_tr']
     model_name = method_list[0]
+    count = 0
     for num_unit in hyperparameters['num_units']:
         for lr in hyperparameters['lrs']:
             for alpha1 in hyperparameters['alphas']:
                 for num_grid1 in hyperparameters['num_grids']:
+                    count += 1
+                    if count <= 0:
+                        continue
                     Mise = []
                     
                     num_grid = num_grid1
@@ -193,16 +196,16 @@ if __name__ == "__main__":
                         init_lr = lr
                         alpha = alpha1
                         tr_init_lr = 0.001
-                        beta = 1.
+                        beta = 0.0
                     np.random.seed(3)
                     for rep in range(replications):
                         torch.manual_seed(3)
-                        x,t,y,v = load_data(dataset)
+                        x,t,y,ids = load_data(dataset)
                         cfg_density = [(x.shape[1], num_unit, 1, 'relu'), (num_unit, num_unit, 1, 'relu')]
-                        data_tr, data_te = data_split(x,t,y, test_ratio)
+                        data_tr, data_te = data_split(x,t,y,ids, test_ratio)
                         data_tr = DataLoader(createDS(data_tr), batch_size=batch_size, shuffle=True)
                         data_te = DataLoader(createDS(data_te), batch_size=1, shuffle=False)
-                        model = Vcnet(cfg_density, num_grid, cfg, degree, knots)
+                        model = Vcnet(cfg_density, num_grid, cfg, degree, knots, dataset=dataset)
                         model._initialize_weights()
                         optimizer = torch.optim.SGD(model.parameters(), lr=init_lr, momentum=momentum, weight_decay=wd, nesterov=True)
 
@@ -244,75 +247,14 @@ if __name__ == "__main__":
                             if cum_loss < best_loss:
                                 best_loss = cum_loss
                                 best_epoch = epoch
-                                torch.save(model.state_dict(), './main_rep' + str(rep) + '.pt')
+                                torch.save(model.state_dict(), 'main_rep' + str(rep) + '.pt')
                             if early_stop(epoch, best_epoch, tol=23):
                                 break
-                        model.load_state_dict(torch.load('./main_rep' + str(rep) + '.pt'))
-                        mise = evaluate_model(model, data_te, v)
+                        model.load_state_dict(torch.load('main_rep' + str(rep) + '.pt'))
+                        mise = evaluate_model(model, data_te, response_data)
                         Mise.append(mise)
                     export_result(out_path, Mise, num_unit, lr, alpha1, num_grid1)
 
 
 
-##        if isTargetReg:
-##            t_grid_hat, mse = curve(model, test_matrix, t_grid, targetreg=TargetReg)
-##        else:
-##            t_grid_hat, mse = curve(model, test_matrix, t_grid)
-##
-##        mse = float(mse)
-##        print('current loss: ', float(loss.data))
-##        print('current test loss: ', mse)
-##        print('-----------------------------------------------------------------')
-##
-##        save_checkpoint({
-##            'model': model_name,
-##            'best_test_loss': mse,
-##            'model_state_dict': model.state_dict(),
-##            'TR_state_dict': TargetReg.state_dict() if isTargetReg else None,
-##        }, checkpoint_dir=save_path)
-##
-##        print('-----------------------------------------------------------------')
-##
-##        grid.append(t_grid_hat)
-##        MSE.append(mse)
 
-
-##    if args.plt_adrf:
-##        import matplotlib.pyplot as plt
-##
-##        font1 = {'family' : 'Times New Roman',
-##        'weight' : 'normal',
-##        'size'   : 22,
-##        }
-##
-##        font_legend = {'family' : 'Times New Roman',
-##        'weight' : 'normal',
-##        'size'   : 22,
-##        }
-##        plt.figure(figsize=(5, 5))
-##
-##        c1 = 'gold'
-##        c2 = 'red'
-##        c3 = 'dodgerblue'
-##
-##        truth_grid = t_grid[:,t_grid[0,:].argsort()]
-##        x = truth_grid[0, :]
-##        y = truth_grid[1, :]
-##        plt.plot(x, y, marker='', ls='-', label='Truth', linewidth=4, color=c1)
-##
-##        x = grid[1][0, :]
-##        y = grid[1][1, :]
-##        plt.scatter(x, y, marker='h', label='Vcnet', alpha=1, zorder=2, color=c2, s=20)
-##
-##        x = grid[0][0, :]
-##        y = grid[0][1, :]
-##        plt.scatter(x, y, marker='H', label='Drnet', alpha=1, zorder=3, color=c3, s=20)
-##
-##        plt.yticks(np.arange(-2.0, 1.1, 0.5), fontsize=0, family='Times New Roman')
-##        plt.xticks(np.arange(0, 1.1, 0.2), fontsize=0, family='Times New Roman')
-##        plt.grid()
-##        plt.legend(prop=font_legend, loc='lower left')
-##        plt.xlabel('Treatment', font1)
-##        plt.ylabel('Response', font1)
-##
-##        plt.savefig(save_path + "/Vc_Dr.pdf", bbox_inches='tight')
